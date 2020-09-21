6b38d917da76c77d4aa90e9a7308547c53a9b13a
Sherifat Yunusa <sherifat.yunusa22@gmail.com>
2020-09-21 06:19:02 +0100

    updated mobile javascript

--- app.js
+++ app.js
@@ -1,2 +1,9 @@
 let parent = document.getElementById('shareCon');
-let svg = document.getElementById('svg');
+let svg = document.getElementById('svg')
+const showCase = document.getElementById('showCase');
+showCase.addEventListener('click', function (e) {
+    parent.style.display = 'flex';
+})
+svg.addEventListener('click', function (e) {
+    parent.style.display = 'none';
+})
